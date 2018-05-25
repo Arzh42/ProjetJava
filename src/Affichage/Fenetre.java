84a0ea1f8a932bc3557a821b4b9900674c8c5b7b
Arzh42 <hugo@chataigner.com>
2018-05-25 11:59:46 +0200

    Ajout interface graphique

--- src/Affichage/Fenetre.java
+++ src/Affichage/Fenetre.java
@@ -1,5 +1,6 @@
 package Affichage;
 
+import java.awt.CardLayout;
 import java.awt.GridLayout;
 
 import javax.swing.JButton;
@@ -7,18 +8,34 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Fenetre extends JFrame {
+	JPanel mainPanel;
+	CardLayout cards;
+	JPanel StatPage = new StatPage(this);
 	JPanel FirstPage = new FirstPage(this);
 	JPanel GestionBilletPage = new GestionBilletPage(this);
+	JPanel NouveauBilletPage = new NouveauBilletPage(this);
+	JPanel ConsultationBilletPage = new ConsultationBilletPage(this);
+	JPanel ModifBillet = new ModifBilletPage(this);
+	JPanel AnnulationBilletPage = new AnnulationBilletPage(this);
 	public Fenetre() {
 		this.setTitle("Ultra Gestionnator of Train and Bus Billet");
 		this.setSize(500, 500);
 		this.setLocationRelativeTo(null);
-		this.getContentPane().add(this.FirstPage);
+		this.cards = new CardLayout();
+		this.mainPanel = new JPanel();
+		this.mainPanel.setLayout(this.cards);
+		this.mainPanel.add(FirstPage, "FirstPage");
+		this.mainPanel.add(GestionBilletPage, "GestionBilletPage");
+		this.mainPanel.add(StatPage,"Stat");
+		this.mainPanel.add(NouveauBilletPage, "NouveauBilletPage");
+		this.mainPanel.add(ConsultationBilletPage, "ConsultationBilletPage");
+		this.mainPanel.add(ModifBillet, "ModifBilletPage");
+		this.mainPanel.add(AnnulationBilletPage,"AnnulationBilletPage" );
+		this.setContentPane(this.mainPanel);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     	this.setVisible(true);
 	}
-	public void switchToGestionBillet() {
-		this.getContentPane().removeAll();
-		this.getContentPane().add(this.GestionBilletPage);
+	public void switchPage(String page) {
+		this.cards.show(this.mainPanel,page);
 	}
 }
