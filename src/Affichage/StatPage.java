88082290603c672c940e3b0441a338ae4aefbd08
Arzh42 <hugo@chataigner.com>
2018-05-25 11:59:51 +0200

    Ajout interface graphique

---
+++ src/Affichage/StatPage.java
@@ -0,0 +1,12 @@
+package Affichage;
+
+import javax.swing.JPanel;
+
+public class StatPage extends JPanel {
+	private Fenetre mainFen;
+	public StatPage(Fenetre mainFen) {
+		this.mainFen = mainFen;
+		
+	}
+	
+}
